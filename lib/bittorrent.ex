7d2191ab3088250afdcbe1778fd4b3c76801efcd
Robin Chowdhury <robinch88@gmail.com>
2024-01-27 09:35:07 +0100

    pass Decode bencoded strings

--- lib/bittorrent.ex
+++ lib/bittorrent.ex
@@ -3,11 +3,11 @@ defmodule Bittorrent.CLI do
       case argv do
           ["decode" | [encoded_str | _]] ->
               # You can use print statements as follows for debugging, they'll be visible when running tests.
-              IO.puts("Logs from your program will appear here!")
+              # IO.puts("Logs from your program will appear here!")
 
               # Uncomment this block to pass the first stage
-              # decoded_str = Bencode.decode(encoded_str)
-              # IO.puts(Jason.encode!(decoded_str))
+              decoded_str = Bencode.decode(encoded_str)
+              IO.puts(Jason.encode!(decoded_str))
           [command | _] ->
               IO.puts("Unknown command: #{command}")
               System.halt(1)
