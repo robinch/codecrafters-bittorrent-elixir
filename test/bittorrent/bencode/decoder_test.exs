3a8b5dd610dabd70229e7d62f2a06ac86e1c0052
Robin Chowdhury <robinch88@gmail.com>
2024-01-27 13:54:39 +0100

    refactor decoder

--- test/bittorrent/bencode/decoder_test.exs
+++ test/bittorrent/bencode/decoder_test.exs
@@ -1,56 +1,56 @@
-defmodule Bittorrent.BencodeTest do
+defmodule Bittorrent.Bencode.DecoderTest do
   use ExUnit.Case
 
-  alias Bittorrent.Bencode
+  alias Bittorrent.Bencode.Decoder
 
   describe "decode dictionary" do
     test "with empty dictionary" do
-      assert {:ok, %{}, ""} == Bencode.decode("de")
+      assert {:ok, %{}, ""} == Decoder.decode("de")
     end
 
     test "with simple dictionary" do
       assert {:ok, %{"foo" => "bar", "hello" => 52}, ""} ==
-               Bencode.decode("d3:foo3:bar5:helloi52ee")
+               Decoder.decode("d3:foo3:bar5:helloi52ee")
     end
 
     test "with nested dictionary" do
       assert {:ok, %{"bar" => %{"foo" => 52}}, ""} ==
-               Bencode.decode("d3:bard3:fooi52eee")
+               Decoder.decode("d3:bard3:fooi52eee")
     end
   end
 
   describe "decode list" do
     test "with empty list" do
-      assert {:ok, [], ""} == Bencode.decode("le")
+      assert {:ok, [], ""} == Decoder.decode("le")
     end
 
     test "with simple list" do
-      assert {:ok, ["hello", 52], ""} == Bencode.decode("l5:helloi52ee")
+      assert {:ok, ["hello", 52], ""} == Decoder.decode("l5:helloi52ee")
     end
 
     test "with nested list" do
-      assert {:ok, [[52], [[]]], ""} == Bencode.decode("lli52eelleee")
+      assert {:ok, [[52], [[]]], ""} == Decoder.decode("lli52eelleee")
     end
   end
 
   describe "decode string" do
     test "with simple string" do
-      assert {:ok, "spam", ""} == Bencode.decode("4:spam")
+      assert {:ok, "spam", ""} == Decoder.decode("4:spam")
     end
 
     test "that contains ':'" do
       assert {:ok, "http://bittorrent-test-tracker.codecrafters.io/announce", ""} ==
-               Bencode.decode("55:http://bittorrent-test-tracker.codecrafters.io/announce")
+               Decoder.decode("55:http://bittorrent-test-tracker.codecrafters.io/announce")
     end
   end
 
   describe "decode integers" do
     test "positive integers" do
-      assert {:ok, 123, ""} == Bencode.decode("i123e")
+      assert {:ok, 123, ""} == Decoder.decode("i123e")
     end
 
     test "negative integers" do
-      assert {:ok, -123, ""} == Bencode.decode("i-123e")
+      assert {:ok, -123, ""} == Decoder.decode("i-123e")
     end
   end
 end
