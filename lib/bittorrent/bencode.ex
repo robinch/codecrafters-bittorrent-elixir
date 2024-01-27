21157b578ea7b4dae69d62695e44730be983cd4c
Robin Chowdhury <robinch88@gmail.com>
2024-01-27 11:22:08 +0100

    decode integers

--- lib/bittorrent/bencode.ex
+++ lib/bittorrent/bencode.ex
@@ -1,8 +1,24 @@
 defmodule Bittorrent.Bencode do
   require Logger
 
+  def decode(<<?i, _rest::binary>> = encoded_value)
+      when is_binary(encoded_value) do
+    decode_integer(encoded_value)
+  end
+
   def decode(encoded_value) do
     decode_string(encoded_value)
+  end
+
+  def decode_integer(<<?i, rest::binary>> = encoded_value) do
+    case String.split(rest, "e") do
+      [value, _] ->
+        {:ok, String.to_integer(value)}
+
+      _ ->
+        Logger.error("Incorrect formatted integer #{inspect(encoded_value)}")
+        {:error, :could_not_decode}
+    end
   end
 
   def decode_string(encoded_value) do
--- test/bittorrent/bencode_test.exs
+++ test/bittorrent/bencode_test.exs
@@ -13,5 +13,14 @@ defmodule Bittorrent.BencodeTest do
                Bencode.decode("55:http://bittorrent-test-tracker.codecrafters.io/announce")
     end
   end
+
+  describe "decode integers" do
+    test "positive integers" do
+      assert {:ok, 123} == Bencode.decode("i123e")
+    end
+
+    test "negative integers" do
+      assert {:ok, -123} == Bencode.decode("i-123e")
+    end
   end
 end
