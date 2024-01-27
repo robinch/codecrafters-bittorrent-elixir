2e0e46ae48b6807c2b97eec4fd0470b7256a4045
Robin Chowdhury <robinch88@gmail.com>
2024-01-27 14:08:39 +0100

    calculate info hash

--- lib/bittorrent/cli.ex
+++ lib/bittorrent/cli.ex
@@ -15,8 +15,15 @@ defmodule Bittorrent.CLI do
         content = IO.iodata_to_binary(binary)
 
         {:ok, decoded_content, _} = Bencode.decode(content)
+
+        info_hash =
+          decoded_content["info"]
+          |> Bencode.encode()
+          |> sha1_encode()
+
         IO.puts("Tracker URL: #{decoded_content["announce"]}")
         IO.puts("Length: #{decoded_content["info"]["length"]}")
+        IO.puts("Info Hash: #{info_hash}")
 
       [command | _] ->
         IO.puts("Unknown command: #{command}")
@@ -26,5 +33,11 @@ defmodule Bittorrent.CLI do
         IO.puts("Usage: your_bittorrent.sh <command> <args>")
         System.halt(1)
     end
+  end
+
+  defp sha1_encode(binary) do
+    :crypto.hash(:sha, binary)
+    |> Base.encode16()
+    |> String.downcase()
   end
 end
--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Bittorrent.MixProject do
 
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :crypto]
     ]
   end
 
